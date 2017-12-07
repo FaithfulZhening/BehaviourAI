004a91276a94bc39c5bd9a46f164d56afd2a0ddc
zhening <taylor_fantasy@163.com>
2017-12-07 00:59:46 -0500

    To be fixed: Stop?

--- Assets/Scripts/Finder.cs
+++ Assets/Scripts/Finder.cs
@@ -4,14 +4,47 @@ using UnityEngine;
 
 public class Finder : MonoBehaviour {
 
-	public Transform destinationPoint;
+	//public Transform destinationPoint;
+	public UnityEngine.AI.NavMeshAgent agent;
+
+	private MapGenerator env;
+	private Destination[] destinations;
+	private Vector3 goal = new Vector3(10f,0f,53f);
+	private bool visitedAll;
 	// Use this for initialization
 	void Start () {
-		
+		visitedAll = false;
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		env = transform.parent.GetComponent<MapGenerator> ();
+		destinations = env.destinations;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.GetComponent<UnityEngine.AI.NavMeshAgent> ().destination = destinationPoint.position;
+		//agent.destination = destinationPoint.position;
+		behaviourTree();	
 	}
+
+	void behaviourTree(){
+		visitedAll = true;
+		for (int i = 0; i < 8; i++) {
+			if (System.Math.Abs (transform.position.x - destinations [i].pos.x) < 0.1 &&
+				System.Math.Abs (transform.position.z - destinations [i].pos.z) < 0.1) {
+				destinations [i].visited = true;
+			}
+
+		}
+		for (int i = 0; i < 8; i++) {
+			if (!destinations [i].visited) {
+				visitedAll = false;
+				agent.destination = destinations [i].pos;
+				break;
+			}
+		}
+
+		if (visitedAll) {
+			agent.destination = goal;
+		}
+	}
+
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 	public GameObject wall;
+	public GameObject agent;
 
 	private GameObject wallHolder;
 
-	private int[,] map = new int[21, 76];
+	private int[,] map = new int[24, 76];
+
+	public Destination[] destinations= new Destination[8];
 	// Use this for initialization
 	void Start () {
 		generateWallPosition ();
 		generateWall ();
+		generateAgent ();
 	}
 	
 	// Update is called once per frame
@@ -24,22 +28,33 @@ public class MapGenerator : MonoBehaviour {
 		wallHolder = new GameObject ("walls");
 		wallHolder.transform.parent = gameObject.transform;	
 		GameObject tempWall;
-		for (int i = 0; i < 21; i++) {
+		int desIndex = 0;
+		for (int i = 0; i < 24; i++) {
 			for (int j = 0; j <= 56; j++) {
 				if (map [i, j] == 1) {
 					tempWall = Instantiate (wall, new Vector3 (i, 0.5f, j), Quaternion.identity);
 					tempWall.tag = "wall";
 					tempWall.transform.parent = wallHolder.transform;
 				}
+				if (map [i, j] == 2) {
+					destinations [desIndex] = new Destination(i, j);
+					desIndex++;
+				}
 			}
 		}
 
 	}
 
+	void generateAgent(){
+		int index = Random.Range(0,8);
+		agent = Instantiate (agent, destinations [index].pos, Quaternion.identity);
+		agent.transform.parent = gameObject.transform;
+	}
+
 	//this method is used to generate the position of the wall
 	void generateWallPosition(){
-		for (int i = 0; i < 21; i++) {
-			if (i == 0 || i == 20) {
+		for (int i = 0; i < 24; i++) {
+			if (i == 0 || i == 23) {
 				for (int j = 1; j <= 4; j++) {
 					map [i, j * 9] = 1;
 					map [i, j * 9 + 1] = 1;
@@ -48,13 +63,19 @@ public class MapGenerator : MonoBehaviour {
 					map [i, j * 9 + 4] = 1;
 				}
 			}
-			if (i < 6 || i > 15) {
+			if (i == 1 || i == 22){
+				for (int j = 1; j <= 4; j++) {
+					map [i, j * 9 + 2] = 2;
+				}
+				
+			}
+			if (i < 6 || i > 18) {
 				for (int j = 1; j <= 4; j++) {
 					map [i, j * 9] = 1;
 					map [i, j * 9 + 4] = 1;
 				}
 			}
-			if (i == 5 || i == 16 ){
+			if (i == 5 || i == 18 ){
 				for (int j = 0; j <= 56; j++) {
 					map [i, j] = 1;
 				}
@@ -66,5 +87,15 @@ public class MapGenerator : MonoBehaviour {
 			}
 
 		}
+	}
+
+
+
+}
+public class Destination{
+	public Vector3 pos;
+	public bool visited = false;
+	public Destination(int xPos, int yPos){
+		this.pos = new Vector3 ((float)xPos, 0, (float)yPos);
 	}
 }
